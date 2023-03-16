abf3977a93ebecac8119b88611234d6773c8a8a6
Nelkha <maritogallego@hotmail.com>
2023-03-15 23:00:16 -0300

    Initial commit

---
+++ src/main/java/com/aguara/aguaraenvios/AguaraEnvios.java
@@ -0,0 +1,17 @@
+
+
+package com.aguara.aguaraenvios;
+
+import com.aguara.aguaraenvios.Views.Principal;
+
+/**
+ *
+ * @author Nelkha
+ */
+public class AguaraEnvios {
+
+    public static void main(String[] args) {
+       Principal ventana = new Principal();
+       ventana.setVisible(true);
+    }
+}
